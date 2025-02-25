cd870de0381b108ce1ad36c7f3eeea98083314a1
getcerts1 <barrowtech1999@gmail.com>
2025-02-25 20:36:28 +0300

    decided to deploy services step by step to avoid errors, started with deploying the function app to the abi subnet through vnet integration

--- backend.tf
+++ backend.tf
@@ -1,118 +1,31 @@
-###  VNet Integration for Function App  ###
+#In this file we will create the function app, the cloud storage dependency and the postgres db
 
-resource "azurerm_storage_account" "cloud_api_storage" {
-  name                     = "linuxfunctionappsa129"
+
+resource "azurerm_storage_account" "cloudstorage129" {
+  name                     = "cloudstorage129"
   resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
+  location                 = var.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
 
-resource "azurerm_service_plan" "api_linux_service_plan" {
-  name                = "my-app-service-plan"
+resource "azurerm_service_plan" "linuxserviceplan129" {
+  name                = "linuxserviceplan129"
   resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  location            = var.location
   os_type             = "Linux"
-  sku_name            = "Y1"
+  sku_name            = "S1"
 }
 
-resource "azurerm_linux_function_app" "my_linux_function_app" {
-  name                = "my-function-app129"
+resource "azurerm_linux_function_app" "functionapp129" {
+  name                = "linxufunctionapp129"
   resource_group_name = azurerm_resource_group.rg.name
   location            = var.location
 
-  storage_account_name       = azurerm_storage_account.cloud_api_storage.name
-  storage_account_access_key = azurerm_storage_account.cloud_api_storage.primary_access_key
-  service_plan_id            = azurerm_service_plan.api_linux_service_plan.id
+  storage_account_name       = azurerm_storage_account.cloudstorage129.name
+  storage_account_access_key = azurerm_storage_account.cloudstorage129.primary_access_key
+  service_plan_id            = azurerm_service_plan.linuxserviceplan129.id
+
   virtual_network_subnet_id = azurerm_subnet.api-subnet.id
-
-  site_config {
-    vnet_route_all_enabled = true # Ensure all traffic follows the VNet routes
-  }
-
-
-}
-
-# ✅ Private Endpoint for Storage Account
-resource "azurerm_private_endpoint" "storage_private_endpoint" {
-  name                = "storage-private-endpoint"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id          = azurerm_subnet.storage_private_endpoint_subnet.id
-
-  private_service_connection {
-    name                           = "storage-connection"
-    private_connection_resource_id = azurerm_storage_account.cloud_api_storage.id
-    subresource_names              = ["blob"]
-    is_manual_connection           = false
-  }
-}
-
-### Private Endpoint for PostgreSQL ###
-resource "azurerm_postgresql_server" "postgres_db" {
-  name                = "my-postgres-db"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = var.location
-  sku_name            = "B_Gen5_2"
-  storage_mb          = 5120
-  version             = "11"
-  administrator_login = "adminuser"
-  administrator_login_password = var.sql_admin_password
-  ssl_enforcement_enabled = false
-}
-
-resource "azurerm_private_endpoint" "postgres_private_endpoint" {
-  name                = "postgres-private-endpoint"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  subnet_id          = azurerm_subnet.sql_private_endpoint_subnet.id
-
-  private_service_connection {
-    name                           = "postgres-connection"
-    private_connection_resource_id = azurerm_postgresql_server.postgres_db.id
-    subresource_names              = ["postgresqlServer"]
-    is_manual_connection           = false
-  }
-}
-
-### Private DNS for Storage Account ###
-resource "azurerm_private_dns_zone" "storage_dns" {
-  name                = "privatelink.blob.core.windows.net"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "storage_dns_link" {
-  name                  = "storage-dns-link"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.storage_dns.name
-  virtual_network_id    = azurerm_virtual_network.spoke_vnet.id
-}
-
-resource "azurerm_private_dns_a_record" "storage_dns_record" {
-  name                = "storage"
-  zone_name           = azurerm_private_dns_zone.storage_dns.name
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.storage_private_endpoint.private_service_connection[0].private_ip_address]
-}
-
-# ✅ Private DNS for PostgreSQL
-resource "azurerm_private_dns_zone" "postgres_dns" {
-  name                = "privatelink.postgres.database.azure.com"
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_private_dns_zone_virtual_network_link" "postgres_dns_link" {
-  name                  = "postgres-dns-link"
-  resource_group_name   = azurerm_resource_group.rg.name
-  private_dns_zone_name = azurerm_private_dns_zone.postgres_dns.name
-  virtual_network_id    = azurerm_virtual_network.spoke_vnet.id
-}
-
-resource "azurerm_private_dns_a_record" "postgres_dns_record" {
-  name                = "postgres"
-  zone_name           = azurerm_private_dns_zone.postgres_dns.name
-  resource_group_name = azurerm_resource_group.rg.name
-  ttl                 = 300
-  records             = [azurerm_private_endpoint.postgres_private_endpoint.private_service_connection[0].private_ip_address]
+  site_config {}
 }
--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "rg" {
   location = var.location
 }
 
-# Hub VNet - Hosts App Gateway and Bastion
+###  HUB VNET ### - Hosts App Gateway and Bastion
 resource "azurerm_virtual_network" "hub_vnet" {
   name                = "hub-vnet"
   location            = azurerm_resource_group.rg.location
@@ -18,31 +18,43 @@ resource "azurerm_subnet" "appgw_subnet" {
   address_prefixes     = ["10.0.0.0/24"]
 }
 
-# Spoke VNet - Hosts Backend Services
-resource "azurerm_virtual_network" "spoke_vnet" {
+
+
+
+### --- SPOKE VNET --- ###          Hosts Backend Services
+
+resource "azurerm_virtual_network" "backend_vnet" {
   name                = "spoke-vnet"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   address_space       = ["10.1.0.0/16"]
 }
 
+### HOSTS THE FUNCTION APP ###
 resource "azurerm_subnet" "api-subnet" {
   name                 = "API-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.spoke_vnet.name
+  virtual_network_name = azurerm_virtual_network.backend_vnet.name
   address_prefixes     = ["10.1.1.0/24"]
+
+  delegation {
+    name = "functionapp-delegation"
+
+    #VNET INTEGRATION ALLOWS FUNCTION APP TO COMMUNICATE PRIVATELY WITH POSTGRES VIA THE NIC
+    service_delegation {
+      name = "Microsoft.Web/serverFarms"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action",
+        "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"
+      ]
+    }
+  }
 }
 
+### HOSTS THE PRIVATE ENDPOINT FOR MY POSTGRES DATABASE ###
 resource "azurerm_subnet" "sql_private_endpoint_subnet" {
   name                 = "SQL-private-endpoint-subnet"
   resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.spoke_vnet.name
+  virtual_network_name = azurerm_virtual_network.backend_vnet.name
   address_prefixes     = ["10.1.2.0/24"]
-}
-
-resource "azurerm_subnet" "storage_private_endpoint_subnet" {
-  name                 = "Storage-private-endpoint-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.spoke_vnet.name
-  address_prefixes     = ["10.1.3.0/24"]
 }
